10c193b3a035ae69569d82d791d8e6ab3815a3d7
vinay <mhvinay5@gmail.com>
2025-06-04 17:07:28 +0530

    2nd commit

--- src/server.js
+++ src/server.js
@@ -2,12 +2,18 @@ import express from "express";
 import dotenv from "dotenv";
 import { initDB } from "./config/db.js";
 import transactionsRoute from "./routes/transactionsRoute.js";
+import job from "./config/cron.js";
 dotenv.config();
 const PORT = process.env.PORT || 5001;
 
-
+if(process.env.NODE_ENV === "production"){
+    job.start();
+}
 const app = express();
 //middleware
+app.get("/api/health", (req, res) => {
+    res.status(200).json({status: "OK"});
+})
 app.use(express.json());
 app.use("/api/transactions", transactionsRoute);
 initDB().then(() => {
